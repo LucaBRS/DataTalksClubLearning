d9b17e172adafbd203b0cf4192f18996f0f01eef
Luca Barsottini <54040929+LucaBRS@users.noreply.github.com>
2026-01-15 14:45:51 +0000

    general testing with uv

--- data-engineering-zoomcamp/docker-workshop/pipelines/pipeline.py
+++ data-engineering-zoomcamp/docker-workshop/pipelines/pipeline.py
@@ -7,6 +7,10 @@ print(f"arguments: {sys.argv}")
 month = int(sys.argv[1])
 
 df = pd.DataFrame({"A":[1,2],"B":[3,4]})
+df['month'] = month
+print(df.head())
+
+df.to_parquet('test.parquet')
 
 
 print(f'hello pipeline, month: {month} ')
