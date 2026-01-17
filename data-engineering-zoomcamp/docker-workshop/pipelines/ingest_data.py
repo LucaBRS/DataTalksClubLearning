b77060671542731e866e6208d426d957d45f6e10
Luca Barsottini <54040929+LucaBRS@users.noreply.github.com>
2026-01-17 22:41:16 +0000

    pipeline working

--- data-engineering-zoomcamp/docker-workshop/pipelines/ingest_data.py
+++ data-engineering-zoomcamp/docker-workshop/pipelines/ingest_data.py
@@ -1,11 +1,7 @@
 import pandas as pd
+from tqdm.auto import tqdm
+from sqlalchemy import create_engine
 
-# parameters
-year = 2021
-month = 1
-
-prefix = 'https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow/'
-url =   f'{prefix}yellow_tripdata_{year}-{month:02d}.csv.gz'
 
 dtype = {
     "VendorID": "Int64",
@@ -23,89 +19,67 @@ dtype = {
     "tolls_amount": "float64",
     "improvement_surcharge": "float64",
     "total_amount": "float64",
-    "congestion_surcharge": "float64"
+    "congestion_surcharge": "float64",
 }
 
-parse_dates = [
-    "tpep_pickup_datetime",
-    "tpep_dropoff_datetime"
-]
-
-pg_user ='root'
-pg_psw='root'
-pg_host='localhost'
-pg_port=5432
-pg_db='ny_taxy'
-
-# # Read a sample of the data
-# prefix = 'https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow/'
-# df = pd.read_csv(prefix + 'yellow_tripdata_2021-01.csv.gz', nrows=100)
-# in this way i am not able to telle pandas what type of data it is so we will ifxx it in the next
-# code section!
+parse_dates = ["tpep_pickup_datetime", "tpep_dropoff_datetime"]
 
 
+def run():
+
+    year = 2021
+    month = 1
+
+    prefix = "https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow/"
+    url = f"{prefix}yellow_tripdata_{year}-{month:02d}.csv.gz"
+
+    pg_user = "root"
+    pg_psw = "root"
+    pg_host = "localhost"
+    pg_port = 5432
+    pg_db = "ny_taxi"
+
+    target_table = "yellow_taxi_data"
+    chunksize = 100000
+    # # Read a sample of the data
+    # prefix = 'https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow/'
+    # df = pd.read_csv(prefix + 'yellow_tripdata_2021-01.csv.gz', nrows=100)
+    # in this way i am not able to telle pandas what type of data it is so we will ifxx it in the next
+    # code section!
+
+    df = pd.read_csv(url, nrows=100, dtype=dtype, parse_dates=parse_dates)
+
+    # this will connect to the db running on port 5432 docker or not
+    engine = create_engine(
+        f"postgresql://{pg_user}:{pg_psw}@{pg_host}:{pg_port}/{pg_db}"
+    )
+
+    # this will show basically the schema the sql alchemy is going to input in psql
+
+    # print(pd.io.sql.get_schema(df, name=target_table, con=engine))
+
+    # what i want so far is just create a table no insert data so i do df.head(0) and it will take
+    # the columns names
+    # is going to return 0 for success
+
+    df_iter = pd.read_csv(
+        url,
+        dtype=dtype,
+        parse_dates=parse_dates,
+        iterator=True,
+        chunksize=chunksize,
+    )
+    first = True
+
+    # is not possible to insert all the df once it's to long and we do not know if is going to be ok
+    # so the solution is to divide it into chunks!!
+    for df_chunk in tqdm(df_iter):
+        if first:
+            df_chunk.head(0).to_sql(name=target_table, con=engine, if_exists="replace")
+            first = False
+
+        df_chunk.to_sql(name=target_table, con=engine, if_exists="append")
 
 
-
-df = pd.read_csv(
-    url',
-    nrows=100,
-    dtype=dtype,
-    parse_dates=parse_dates
-)
-df.head()
-
-
-
-
-from sqlalchemy import create_engine
-# this will connect to the db running on port 5432 docker or not
-engine = create_engine(f'postgresql://{pg_user}:{pg_psw}@{pg_host}:{pg_port}/{pg_db}')
-
-
-
-
-# this will show basically the schema the sql alchemy is going to input in psql
-print(pd.io.sql.get_schema(df, name='yellow_taxi_data', con=engine))
-
-
-# In[32]:
-
-
-# what i want so far is just create a table no insert data so i do df.head(0) and it will take
-# the columns names
-# is going to return 0 for success
-df.head(0).to_sql(name='yello_taxy_data', con=engine, if_exists='replace')
-
-
-# In[33]:
-
-
-# is not possible to insert all the df once it's to long and we do not know if is going to be ok
-# so the solution is to divide it into chunks!!
-df_iterator = pd.read_csv(url,
-                          dtype = dtype,
-                          parse_dates=parse_dates,
-                          iterator=True,
-                          chunksize=100000
-                         )
-
-
-# In[34]:
-
-
-from tqdm.auto import tqdm
-
-
-# In[35]:
-
-
-for df_chunk in tqdm(df_iterator):
-    df_chunk.to_sql(name='yello_taxy_data', con=engine, if_exists='append')
-
-
-# In[ ]:
-
-
-
-
+if __name__ == "__main__":
+    run()
