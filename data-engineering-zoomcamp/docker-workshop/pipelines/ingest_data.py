09a1f49ce4731cd02113ea91082054fb5970edee
Luca Barsottini <54040929+LucaBRS@users.noreply.github.com>
2026-01-18 16:55:05 +0000

    docker compose

--- data-engineering-zoomcamp/docker-workshop/pipelines/ingest_data.py
+++ data-engineering-zoomcamp/docker-workshop/pipelines/ingest_data.py
@@ -1,7 +1,10 @@
-import pandas as pd
-from tqdm.auto import tqdm
-from sqlalchemy import create_engine
+#!/usr/bin/env python
+# coding: utf-8
 
+import click
+import pandas as pd
+from sqlalchemy import create_engine
+from tqdm.auto import tqdm
 
 dtype = {
     "VendorID": "Int64",
@@ -25,42 +28,29 @@ dtype = {
 parse_dates = ["tpep_pickup_datetime", "tpep_dropoff_datetime"]
 
 
-def run():
+# click is used to create command line interface!
+@click.command()
+@click.option("--pg-user", default="root", help="PostgreSQL user")
+@click.option("--pg-pass", default="root", help="PostgreSQL password")
+@click.option("--pg-host", default="localhost", help="PostgreSQL host")
+@click.option("--pg-port", default=5432, type=int, help="PostgreSQL port")
+@click.option("--pg-db", default="ny_taxi", help="PostgreSQL database name")
+@click.option("--year", default=2021, type=int, help="Year of the data")
+@click.option("--month", default=1, type=int, help="Month of the data")
+@click.option("--target-table", default="yellow_taxi_data", help="Target table name")
+@click.option(
+    "--chunksize", default=100000, type=int, help="Chunk size for reading CSV"
+)
+def run(
+    pg_user, pg_pass, pg_host, pg_port, pg_db, year, month, target_table, chunksize
+):
+    """Ingest NYC taxi data into PostgreSQL database."""
+    prefix = "https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow"
+    url = f"{prefix}/yellow_tripdata_{year}-{month:02d}.csv.gz"
 
-    year = 2021
-    month = 1
-
-    prefix = "https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow/"
-    url = f"{prefix}yellow_tripdata_{year}-{month:02d}.csv.gz"
-
-    pg_user = "root"
-    pg_psw = "root"
-    pg_host = "localhost"
-    pg_port = 5432
-    pg_db = "ny_taxi"
-
-    target_table = "yellow_taxi_data"
-    chunksize = 100000
-    # # Read a sample of the data
-    # prefix = 'https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow/'
-    # df = pd.read_csv(prefix + 'yellow_tripdata_2021-01.csv.gz', nrows=100)
-    # in this way i am not able to telle pandas what type of data it is so we will ifxx it in the next
-    # code section!
-
-    df = pd.read_csv(url, nrows=100, dtype=dtype, parse_dates=parse_dates)
-
-    # this will connect to the db running on port 5432 docker or not
     engine = create_engine(
-        f"postgresql://{pg_user}:{pg_psw}@{pg_host}:{pg_port}/{pg_db}"
+        f"postgresql://{pg_user}:{pg_pass}@{pg_host}:{pg_port}/{pg_db}"
     )
-
-    # this will show basically the schema the sql alchemy is going to input in psql
-
-    # print(pd.io.sql.get_schema(df, name=target_table, con=engine))
-
-    # what i want so far is just create a table no insert data so i do df.head(0) and it will take
-    # the columns names
-    # is going to return 0 for success
 
     df_iter = pd.read_csv(
         url,
@@ -69,10 +59,9 @@ def run():
         iterator=True,
         chunksize=chunksize,
     )
+
     first = True
 
-    # is not possible to insert all the df once it's to long and we do not know if is going to be ok
-    # so the solution is to divide it into chunks!!
     for df_chunk in tqdm(df_iter):
         if first:
             df_chunk.head(0).to_sql(name=target_table, con=engine, if_exists="replace")
