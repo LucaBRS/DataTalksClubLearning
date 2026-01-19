8acdc9ceddd63b0b1662096a39887bcb2bc76991
Luca Barsottini <54040929+LucaBRS@users.noreply.github.com>
2026-01-19 17:25:24 +0000

    start tf

--- data-engineering-zoomcamp/docker-workshop/terraform_test/main.tf
+++ data-engineering-zoomcamp/docker-workshop/terraform_test/main.tf
@@ -1,14 +1,15 @@
-terraform{
-    required_providers {
-      google = {
-        source = "hashicorp/google"
-        version = "5.6.0"
-      }
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "5.6.0"
     }
+  }
 }
 
 provider "google" {
-    # config option
-    project = ""
-    region = ""
+  # config option
+  credentials =
+  project = "test-data-eng-course"
+  region  = "europe-west8"
 }
